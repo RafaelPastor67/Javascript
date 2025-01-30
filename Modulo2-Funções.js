deb77897eb41040e67eb9622fd0a3d8080dc58fa
RafaelPastor67 <166877002+RafaelPastor67@users.noreply.github.com>
2025-01-30 16:13:37 -0300

    Initial commit

---
+++ Modulo2-Funções.js
@@ -0,0 +1,21 @@
+function soma(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mult(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
+function MostraResultado(){
+    a= 15
+    b= 5
+    console.log("Soma: " + soma(a,b));
+    console.log("Subtração:" + sub(a,b));
+    console.log("Multiplicação:" + mult(a,b));
+    console.log("Divisão:" + div(a,b));
+}
+MostraResultado();
